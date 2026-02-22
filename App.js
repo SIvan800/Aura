3e8e04e94306ce70e649e0dcf1ab5327fccd1333
Sivan Cohen <--@example.com>
2026-02-22 22:07:26 +0200

    Feat: setup basic layout with styled boxes and nested text

--- App.js
+++ App.js
@@ -4,7 +4,16 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>hello Aura</Text>
+      <Text>Good Morning</Text>
+      <View style={styles.box1}>
+        <Text style={styles.boxText}>Zen</Text>
+      </View>
+      <View style={styles.box2}>
+        <Text style={styles.boxText}>Spark</Text>
+      </View>
+      <View style={styles.box3}>
+        <Text style={styles.boxText}>Discovery</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +22,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'column',
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
+  },
+  box1: {
+    width: 200, height: 100, backgroundColor: '#ffcfd2',
+  },
+  box2: {
+    width: 200, height: 100, backgroundColor: '#fcf5c7',
+  },
+  box3: {
+    width: 200, height: 100, backgroundColor: '#D9E9CF',
   },
 });
